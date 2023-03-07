ff99753dc5398f398c7211ac938d4bdcd3f4f8d1
nuffens <55536943+nuffens@users.noreply.github.com>
2023-03-07 17:53:05 -0500

    Add files via upload

---
+++ bottles.cob
@@ -0,0 +1,22 @@
+                  >>source format free
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. BOTTLES.
+       AUTHOR. Amber.
+       DATE-WRITTEN. MAR 7.
+       ENVIRONMENT DIVISION.
+       
+       DATA DIVISION.
+       
+       WORKING-STORAGE SECTION.
+       01 WS-I PIC 99 VALUE 99.
+       01 WS-DUMP PIC 99.
+       
+       PROCEDURE DIVISION.
+              PERFORM UNTIL WS-I = 0
+              ADD -1 TO WS-I
+              IF WS-I = 0 DISPLAY "NO MORE BEER"
+              ELSE DISPLAY WS-I " BOTTLES OF BEER ON THE WALL " WS-I
+              " BOTTLES OF BEER "
+              END-PERFORM.
+       STOP RUN.
+       END PROGRAM BOTTLES.
